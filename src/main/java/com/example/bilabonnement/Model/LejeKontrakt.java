f2da9ecfcc1cd767985f035aec257268b20ffe72
Lina Al-Hajjar <lial0001@stud.kea.dk>
2024-11-29 18:24:45 +0100

    Kunde and LejeKontrakt klasser: DONE. der manglede 2 ";" i BilabonnementRepo klassen

--- src/main/java/com/example/bilabonnement/Model/LejeKontrakt.java
+++ src/main/java/com/example/bilabonnement/Model/LejeKontrakt.java
@@ -1,4 +1,81 @@
 package com.example.bilabonnement.Model;
 
+import java.time.LocalDate;
+
 public class LejeKontrakt {
+    int lejekontrakt_id;
+    String telefonnummer;
+    String nummerplade;
+    LocalDate startdato;
+    LocalDate slutdato;
+    int maxKm;
+    double pris;
+
+    public LejeKontrakt(){}
+
+    public LejeKontrakt(int lejekontrakt_id, double pris, int maxKm, LocalDate slutdato, LocalDate startdato, String nummerplade, String telefonnummer) {
+        this.lejekontrakt_id = lejekontrakt_id;
+        this.pris = pris;
+        this.maxKm = maxKm;
+        this.slutdato = slutdato;
+        this.startdato = startdato;
+        this.nummerplade = nummerplade;
+        this.telefonnummer = telefonnummer;
+    }
+
+    public int getLejekontrakt_id() {
+        return lejekontrakt_id;
+    }
+
+    public void setLejekontrakt_id(int lejekontrakt_id) {
+        this.lejekontrakt_id = lejekontrakt_id;
+    }
+
+    public String getTelefonnummer() {
+        return telefonnummer;
+    }
+
+    public void setTelefonnummer(String telefonnummer) {
+        this.telefonnummer = telefonnummer;
+    }
+
+    public String getNummerplade() {
+        return nummerplade;
+    }
+
+    public void setNummerplade(String nummerplade) {
+        this.nummerplade = nummerplade;
+    }
+
+    public LocalDate getStartdato() {
+        return startdato;
+    }
+
+    public void setStartdato(LocalDate startdato) {
+        this.startdato = startdato;
+    }
+
+    public LocalDate getSlutdato() {
+        return slutdato;
+    }
+
+    public void setSlutdato(LocalDate slutdato) {
+        this.slutdato = slutdato;
+    }
+
+    public int getMaxKm() {
+        return maxKm;
+    }
+
+    public void setMaxKm(int maxKm) {
+        this.maxKm = maxKm;
+    }
+
+    public double getPris() {
+        return pris;
+    }
+
+    public void setPris(double pris) {
+        this.pris = pris;
+    }
 }
