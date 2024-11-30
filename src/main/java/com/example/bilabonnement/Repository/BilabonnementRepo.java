e3d381af1fa4e045f0d509cfb48bd6c4c9c018f9
YousafTJ <youjav@hotmail.com>
2024-11-30 11:44:10 +0100

    last commit for sprint 1

--- src/main/java/com/example/bilabonnement/Repository/BilabonnementRepo.java
+++ src/main/java/com/example/bilabonnement/Repository/BilabonnementRepo.java
@@ -1,6 +1,8 @@
 package com.example.bilabonnement.Repository;
 
 
+import com.example.bilabonnement.Model.Kunde;
+import com.example.bilabonnement.Model.LejeKontrakt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -12,18 +14,18 @@ public class BilabonnementRepo {
     JdbcTemplate template;
 
 
-    public void addKunde(){
+    public void addKunde(Kunde kunde){
 
-        String sql = "INSERT INTO dummyTable(telefonnummer, email, fornavn, efternavn, adresse, postnummer, by, koerekortnummer, udstedelsdato)\n" +
+        String sql = "INSERT INTO dummyTable(telefonnummer, email, fornavn, efternavn, adresse, postnummer, byen, koerekortnummer, udstedelsdato)\n" +
                 "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);";
-        template.update(sql); // mangler getters og setters:DONE
+        template.update(sql, kunde.getTelefonnummer(), kunde.getEmail(), kunde.getFornavn(), kunde.getEfternavn(), kunde.getAdresse(), kunde.getPostnummer(), kunde.getByen(), kunde.getKoerekortnummer(), kunde.getUdstedelsdato()); // mangler getters og setters:DONE
     }
 
 
-    public void addLejekontrakt(){
-        String sql = "INSERT INTO lejekontrakt(lejekontrakt_id, telefonnummer, nummerplade, startDato, slutDato, maxKm, pris)\n" +
-                "VALUES(?, ?, ?, ?, ?, ?, ?)";
-        template.update(sql); // mangler getters og setters:DONE
+    public void addLejekontrakt(LejeKontrakt lejeKontrakt){
+        String sql = "INSERT INTO lejekontrakt(telefonnummer, nummerplade, startDato, slutDato, maxKm, pris)\n" +
+                "VALUES(?, ?, ?, ?, ?, ?)";
+        template.update(sql, lejeKontrakt.getTelefonnummer(), lejeKontrakt.getNummerplade(), lejeKontrakt.getStartdato(), lejeKontrakt.getSlutdato(), lejeKontrakt.getMaxKm(), lejeKontrakt.getPris()); // denne kode adder til databasen ved hjælp af getters
     }
 
 }
--- src/main/java/com/example/bilabonnement/Service/BilabonnementService.java
+++ src/main/java/com/example/bilabonnement/Service/BilabonnementService.java
@@ -1,5 +1,7 @@
 package com.example.bilabonnement.Service;
 
+import com.example.bilabonnement.Model.Kunde;
+import com.example.bilabonnement.Model.LejeKontrakt;
 import com.example.bilabonnement.Repository.BilabonnementRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,12 +15,12 @@ public class BilabonnementService {
     BilabonnementRepo bilabonnementRepo;
 
 
-    public void addKunde(){
-        bilabonnementRepo.addKunde();
+    public void addKunde(Kunde kunde){
+        bilabonnementRepo.addKunde(kunde);
     }
 
 
-    public void addLejekontrakt(){
-        bilabonnementRepo.addLejekontrakt();
+    public void addLejekontrakt(LejeKontrakt lejeKontrakt){
+        bilabonnementRepo.addLejekontrakt(lejeKontrakt);
     }
 }
