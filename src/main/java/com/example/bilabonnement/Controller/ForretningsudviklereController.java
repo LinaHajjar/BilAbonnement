9de0bce2b18673a814194b60f4c58bc8a64a5ec7
Lina Al-Hajjar <lial0001@stud.kea.dk>
2024-12-10 12:25:55 +0100

    Controller klasser

--- src/main/java/com/example/bilabonnement/Controller/ForretningsudviklereController.java
+++ src/main/java/com/example/bilabonnement/Controller/ForretningsudviklereController.java
@@ -1,4 +1,104 @@
 package com.example.bilabonnement.Controller;
 
+import com.example.bilabonnement.Model.TopBil;
+import com.example.bilabonnement.Service.BilService;
+import com.example.bilabonnement.Service.LejeKontraktService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+@Controller
 public class ForretningsudviklereController {
+
+    @Autowired
+    LejeKontraktService lejeKontraktService;
+
+    @Autowired
+    BilService bilService;
+
+    @GetMapping("/antalLejedeBiler")
+    public String antalLejedeBiler(Model model) {
+        return "homeForretningsUdvikler/antalLejedeBiler";
+    }
+
+    @PostMapping("/antalLejedeBiler")
+    public String antalLejedeBiler( @RequestParam("startdato") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startdato,
+                                    @RequestParam("slutdato") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate slutdato, @RequestParam(value = "maerker",required = false) String selectedMaerke,  Model model) throws SQLException {
+
+        int lejedeBiler = lejeKontraktService.getAntalBiler(startdato,slutdato); //method getTotalLejedeBiler skal laves i repo og service
+        //List<String> maerker = lejeKontraktService.getBilMaerker();
+
+
+        model.addAttribute("lejedeBiler", lejedeBiler);
+        model.addAttribute("startdato", startdato);
+        model.addAttribute("slutdato",slutdato);
+        //model.addAttribute("maerker", maerker);
+        //model.addAttribute("maerke", selectedMaerke);
+        return "homeForretningsUdvikler/antalLejedeBiler";
+    }
+
+    @GetMapping("/samletIndtægt")
+    public String samletIndtægt(Model model) {
+        return "homeForretningsUdvikler/samletIndtægt";
+    }
+
+    @PostMapping("/samletIndtægt")
+    public String samletIndtægt(@RequestParam("fraDato")LocalDate fraDato,@RequestParam("tilDato")LocalDate tilDato,  Model model)throws SQLException{
+
+        Double samletIndtægt = bilService.getSamletIndtægt(fraDato, tilDato); //method getSamletIndtægter skal laves i repo og service
+
+        if (samletIndtægt == null || samletIndtægt == 0.0){
+            model.addAttribute("ingenIndtægt", "Der er ikke nogen indtægt indenfor denne dato.");
+        } else {
+            model.addAttribute("samletIndtægt", samletIndtægt);
+        }
+
+
+        model.addAttribute("fraDato", fraDato);
+        model.addAttribute("tilDato", tilDato);
+
+        return "homeForretningsUdvikler/samletIndtægt";
+    }
+
+    @GetMapping("/topLejedeModeller")
+    public String topLejedeModeller(){
+        return "homeForretningsUdvikler/topLejedeModeller";
+    }
+
+    @PostMapping("/topLejedeModeller")
+    public String topLejedeModeller(@RequestParam("fraDato") LocalDate fraDato,
+                                    @RequestParam("tilDato")LocalDate tilDato,  Model model)throws SQLException {
+
+        try {
+            TopBil topLejedeModel = bilService.getTopLejedeModeller(fraDato, tilDato);
+            //System.out.println(topBil);
+            String bilModel = topLejedeModel.getModel();
+            String maerke = topLejedeModel.getMaerke();
+            int antalLånt = topLejedeModel.getAntal();
+            model.addAttribute("model", bilModel);
+            model.addAttribute("maerke", maerke);
+            model.addAttribute("antalLånt", antalLånt);
+
+            return "homeForretningsUdvikler/topLejedeModeller";
+        } catch (EmptyResultDataAccessException e){
+            model.addAttribute("ingenTopBil", "Der er ikke udlånt nogle biler i denne periode");
+            return "homeForretningsUdvikler/topLejedeModeller";
+
+        }
+    }
+
+
+    @GetMapping("/homeForretningsUdvikler")
+    public String homeForretningsUdvikler(){
+        return "homeForretningsUdvikler/forretningsUdvikler";
+    }
+
 }
--- src/main/java/com/example/bilabonnement/Controller/LejekontraktController.java
+++ src/main/java/com/example/bilabonnement/Controller/LejekontraktController.java
@@ -1,4 +1,147 @@
 package com.example.bilabonnement.Controller;
 
+import com.example.bilabonnement.Model.LejeKontrakt;
+import com.example.bilabonnement.Service.BilService;
+import com.example.bilabonnement.Service.LejeKontraktService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller
 public class LejekontraktController {
+
+    @Autowired
+    BilService bilService;
+    @Autowired
+    LejeKontraktService lejeKontraktService;
+
+    // oprettelse af getmapping metode for at vise alle lejekontrakter
+    @GetMapping("/manageKontrakter")
+    public String allLejeKontrakter(Model model) throws SQLException {
+        model.addAttribute("lejekontrakter", lejeKontraktService.getAllLejeKontrakt());
+        return "homeKontrakt/manageKontrakter";
+    }
+
+    //oprettelse af postmaping metode for at sende input fra bruger omkring lejekontrakt info
+
+
+    @GetMapping("/nyKontrakt")
+    public String nyKontrakt(Model model) throws SQLException {
+        List<String> nummerpladeList = bilService.alleNummerplader();
+        model.addAttribute("nummerplader", nummerpladeList);
+        return "homeKontrakt/nyKontrakt";
+    }
+
+    @PostMapping("/nyKontrakter")
+    public String visLejeKontraktForm(@RequestParam("telefonnummer") String telefonnummer,
+                                      @RequestParam("nummerplade") String nummerplade,
+                                      @RequestParam("startdato") LocalDate startdato,
+                                      @RequestParam("slutdato") LocalDate slutdato,
+                                      @RequestParam("maxKm") int maxKm,
+                                      @RequestParam("pris") int pris,
+                                      Model model) throws SQLException {
+
+        LejeKontrakt kontrakt = new LejeKontrakt(); // der oprettes en lejekontrakt object med info fra brugeren
+        kontrakt.setTelefonnummer(telefonnummer);
+        kontrakt.setNummerplade(nummerplade);
+        kontrakt.setStartdato(startdato);
+        kontrakt.setSlutdato(slutdato);
+        kontrakt.setMaxKm(maxKm);
+        kontrakt.setPris(pris);
+        lejeKontraktService.addLejekontrakt(kontrakt); // her sendes den videre til addlejekontrakt metoden
+        return "redirect:/manageKontrakter";
+    }
+
+    @GetMapping("/nummerplade")
+    public String nummerplade(Model model) throws SQLException {
+        return "homeKontrakt/nyKontrakt";
+    }
+
+
+
+
+    @PostMapping("/tilføjKontrakt")
+    public String tilføjKontrakt(@RequestParam("telefonnummer") String telefonnummer,
+                                 @RequestParam("nummerplade") String nummerplade,
+                                 @RequestParam("startdato") LocalDate startdato,
+                                 @RequestParam("slutdato") LocalDate slutdato,
+                                 @RequestParam("maxKm") int maxKm,
+                                 @RequestParam("pris") int pris,
+                                 RedirectAttributes redirectAttributes) throws SQLException {
+
+        try {
+
+            LejeKontrakt kontrakt = new LejeKontrakt();
+            kontrakt.setTelefonnummer(telefonnummer);
+            kontrakt.setNummerplade(nummerplade);
+            kontrakt.setStartdato(startdato);
+            kontrakt.setSlutdato(slutdato);
+            kontrakt.setMaxKm(maxKm);
+            kontrakt.setPris(pris);
+            lejeKontraktService.addLejekontrakt(kontrakt);
+            redirectAttributes.addFlashAttribute("oprettet", "Lejekontrakt er blevet oprettet."); // giver en message hvis lejekontrakt bliver oprettet
+            return "redirect:/manageKontrakter";
+        } catch (DataIntegrityViolationException ex) {
+            // Handle the exception and add an error message
+            redirectAttributes.addFlashAttribute("error",
+                    "Systemet kunne ikke oprette kontrakten. Tjek om telefonnummeret og nummerpladen eksisterer"); // giver en message hvis lejekontrakt ikke bliver oprettet
+
+            return "redirect:/manageKontrakter";
+        }
+    }
+
+
+    @PostMapping("/sletKontrakt")
+    public String sletKontrakt(@RequestParam("lejekontrakt_id") int id) throws SQLException {
+        lejeKontraktService.sletLejeKontract(id);
+        return "redirect:/manageKontrakter";
+    }
+
+    // til at søge alle kontrakter til det samme kunde (telefonnummer)
+    @GetMapping("/soegLejekontraktTelefon")
+    public String homemanageKontrakt() {
+        return ("homeKontrakt/manageKontrakter");
+    }
+
+
+    @GetMapping("/backToKontrakt")
+    public String backToKontrakt(Model model) {
+        return "redirect:/manageKontrakter";
+    }
+
+
+
+    @PostMapping("/kundensKontrakter")
+    public String kundensKontrakter(@RequestParam("telefonnummer") String telefonnummer, Model model, RedirectAttributes redirectAttributes) throws SQLException {
+
+        List<LejeKontrakt> kontrakter = lejeKontraktService.findKontraktByTelefon(telefonnummer);
+
+        if (kontrakter == null || kontrakter.isEmpty()){
+
+            redirectAttributes.addFlashAttribute("ingenKontrakt", "der ikke tilknyttet nogle kontrakter til denne kunde, eller er kunden ikke oprettet");
+            redirectAttributes.addFlashAttribute("nyKunde", true);
+            return "redirect:/manageKontrakter";
+        } else {
+            model.addAttribute("kontrakter", kontrakter);
+            return "homeKontrakt/kundensKontrakter";
+        }
+
+    }
+
+
+    @GetMapping("/alleKontrakter")
+    public String alleKontrakter(){
+
+        return "redirect:/manageKontrakter";
+    }
+
 }
