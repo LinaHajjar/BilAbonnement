82f70b1e3b406a5970eae625d31a721589fc7233
Lina Al-Hajjar <lial0001@stud.kea.dk>
2024-12-10 10:41:31 +0100

    test til getSamletIndtægt metode: DONE

--- src/test/java/com/example/bilabonnement/BilAbonnementApplicationTests.java
+++ src/test/java/com/example/bilabonnement/BilAbonnementApplicationTests.java
@@ -3,11 +3,26 @@ package com.example.bilabonnement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+
 @SpringBootTest
 class BilAbonnementApplicationTests {
 
     @Test
     void contextLoads() {
     }
+
+    @Test
+    void getSamletIndtægt() { //A method with @Test is flagged as a JUnit test case.
+        LocalDate fraDato_1 = LocalDate.of(2019, 1, 12);
+        LocalDate tilDato_1 = LocalDate.of(2023, 12, 31);
+        double sumForventet_1 = 16000.0;
+
+        LocalDate fraDato_2 = LocalDate.of(2024, 12, 01);
+        LocalDate tilDato_2 = LocalDate.of(2024, 12, 31);
+        double sumForventet_2 = 4000.0;
+    }
+
+
 
 }
